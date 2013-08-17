e28f8a4ae3bcbd067c15d1f0039e6454dd60975b
hippoakuna <hippoakuna@gmail.com>
2013-08-17 17:58:20 -0500

    Added User follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 HippoArtists::Application.routes.draw do
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   root 'global_pages#home'
+
   match '/legal',   to: 'global_pages#legal', via: 'get'
   match '/signup',  to: 'users#new',          via: 'get'
   match '/signin',  to: 'sessions#new',       via: 'get'
