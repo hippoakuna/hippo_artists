70b41641b39a42a797dae608a78da3d7453b1b23
hippoakuna <hippoakuna@gmail.com>
2013-08-17 15:10:29 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 HippoArtists::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'global_pages#home'
-  match '/legal',  to: 'global_pages#legal', via: 'get'
-  match '/signup', to: 'users#new',          via: 'get'
+  match '/legal',   to: 'global_pages#legal', via: 'get'
+  match '/signup',  to: 'users#new',          via: 'get'
+  match '/signin',  to: 'sessions#new',       via: 'get'
+  match '/signout', to: 'sessions#destroy',   via: 'delete'
 end
